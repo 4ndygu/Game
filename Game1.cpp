84a9e504af724f58d4ace162f634a9af1ba97736
Laura Gouillon <lgouillon@yahoo.com>
2014-12-21 15:01:42 -0800

    Improve Battleship, fix matrix x/y reverse and ShipsLeft bugs

--- Game1.cpp
+++ Game1.cpp
@@ -6,9 +6,10 @@ using namespace std;
 const int rows = 10;
 const int columns = 10;
 int matrix[rows][columns];
-int maxships = 10;
+const int maxships = 10;
 
-void Clear() {	// Set all positions to initial 0
+// Set all positions to initial 0
+void Clear() {	
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			matrix[i][j] = 0;
@@ -16,7 +17,8 @@ void Clear() {	// Set all positions to initial 0
 	}
 }
 
-void Show() {	// Display matrix
+// Display matrix
+void Show() {	
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			cout << matrix[i][j] << " ";
@@ -25,27 +27,30 @@ void Show() {	// Display matrix
 	}
 }
 
-void SetShips() {	// Randomly place ships on matrix
+// Randomly place ships on matrix
+void SetShips() {	
 	int ships = 0;
 	while (ships < maxships) {
 		int x = rand() % rows;
 		int y = rand() % columns;
 		if (matrix[x][y] != 1) {
 			matrix[x][y] = 1;
+			ships++;
 		}
-		ships++;
 	}
 }
 
-bool Attack(int x, int y) {	// Change attacked ship position value and return boolean
-	if (matrix[x][y] == 1) {
-		matrix[x][y] = 2;
+// Change attacked ship position value and return boolean
+bool Attack(int x, int y) {	
+	if (matrix[y][x] == 1) {	// switch spot of x and y to accommodate matrix
+		matrix[y][x] = 2;		// switch spot of x and y to accommodate matrix
 		return true;
 	}
 	return false;
 }
 
-int ShipsLeft() {	// For calculating number of ships remaining
+// For calculating number of ships remaining
+int ShipsLeft() {	
 	int c = 0;
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
@@ -59,29 +64,52 @@ int ShipsLeft() {	// For calculating number of ships remaining
 
 int main() {
 	srand(time(0));
-	Clear();
+
+	// Initialize Game
+	Clear();	
+	cout << "Cleared board: " << endl;
+	Show();
 	SetShips();
-	int pos1, pos2;
-	char prompt;
+	cout << "Ships are set: " << endl;
+	Show();
+	cout << "Number of Ships Left: " << ShipsLeft() << endl;
+
+	int pos1, pos2;	// For user inputs of x and y
+	char prompt;	// For user input of surrender
+
 	while (1) {
+		// Prompt user for x and y coordinates 
 		cout << "Please input position x and y: ";
 		cin >> pos1 >> pos2;
-		if (Attack(pos1, pos2)) {
-			cout << "You got me!" << endl;
-		}
-		else {
-			cout << "Sorry, there are no ships there." << endl;
-		}
-		cout << "Number of ships left: " << ShipsLeft() << endl;
-		if (ShipsLeft() == 0) {
-			cout << "Hooray! You sunk all the ships!" << endl;
+		if (cin.fail()) {	// If incorrect input format, exit game
 			break;
 		}
+
+		bool checkAttack = Attack(pos1, pos2);
+		// Attack successful
+		if (checkAttack) {	
+			cout << "You got me!" << endl;
+		}
+		// Attack not successful
+		else {	
+			cout << "Sorry, there are no ships there." << endl;
+		}
+
+		cout << "Number of ships left: " << ShipsLeft() << endl;
+		if (ShipsLeft() == 0) {
+			cout << "GAMEOVER: Hooray! You sunk all the ships!" << endl;
+			break;
+		}
+
+		// Surrender prompt (y = exit, n = display board and continue)
 		cout << "Do you want to surrender? (y/n): ";
 		cin >> prompt;
 		cout << endl;
 		if (prompt == 'y') {
 			break;
+		}
+		else if (prompt == 'n') {
+			Show();
 		}
 	}
 	Show();
